06af18753a43a3d903e06d9fd1e1354730fbeb53
Alex Gustavo Welz <alex.welz6@gmail.com>
2023-10-05 18:31:28 -0300

    Agrego alerta al enviar mail

--- js/script.js
+++ js/script.js
@@ -60,3 +60,25 @@ const typed =new Typed('.multiple-text',{
     backDelay: 1000,
     loop: true
 });
+
+
+ /*====================== alter mail ===========================*/
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('The mail was sent successfully!', 'success')
+  })
+}
